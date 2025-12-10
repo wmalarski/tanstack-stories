c865960d8442fbf235c992f6adfcf36466f3db7b
Wojtek Malarski <w.malarski@gmail.com>
2025-12-10 20:48:37 +0100

    adds better auth client

--- src/integrations/better-auth/init.ts
+++ src/integrations/better-auth/init.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 import type { Db } from "../drizzle/init";
 import { schema } from "../drizzle/schema";
@@ -11,9 +12,10 @@ type InitBetterAuthArgs = {
 
 export const initBetterAuth = ({ db, env }: InitBetterAuthArgs) => {
   return betterAuth({
-    baseURL: env.BETTER_AUTH_URL,
+    baseURL: env.VITE_BETTER_AUTH_URL,
     database: drizzleAdapter(db, { provider: "sqlite", schema }),
     emailAndPassword: { enabled: true },
+    plugins: [tanstackStartCookies()],
     secret: env.BETTER_AUTH_SECRET,
   });
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
+import { authClient } from "@/integrations/better-auth/client";
+
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+  console.log("[authClient]", authClient);
   return <div>Hello</div>;
 }
