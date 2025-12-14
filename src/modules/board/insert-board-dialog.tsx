93b202b70c2e5c1b1bfbd51d8422d0ecc6bc8359
Wojtek Malarski <w.malarski@gmail.com>
2025-12-14 22:51:50 +0100

    small update

--- src/modules/board/insert-board-dialog.tsx
+++ src/modules/board/insert-board-dialog.tsx
@@ -9,48 +9,16 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-  FieldSet,
-} from "@/components/ui/field";
-import { FormError } from "@/components/ui/form-error";
 import { useAppForm } from "@/integrations/tanstack-form";
 
 import { useMutation } from "@tanstack/react-query";
 
+import { BoardFields } from "./board-fields";
 import { insertBoardMutationOptions } from "./services";
 import { type InsertBoardArgs, InsertBoardSchema } from "./validation";
 
 export const InsertBoardDialog = () => {
-  return (
-    <Dialog>
-      <DialogTrigger render={<Button variant="outline" />}>
-        Open Dialog
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </DialogDescription>
-        </DialogHeader>
-        <InsertBoardForm />
-        <DialogFooter>
-          <DialogClose render={<Button variant="outline" />}>
-            Cancel
-          </DialogClose>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
-};
-
-const InsertBoardForm = () => {
-  const signInMutation = useMutation(insertBoardMutationOptions());
+  const insertBoardMutation = useMutation(insertBoardMutationOptions());
 
   const insertBoardForm = useAppForm({
     defaultValues: {
@@ -58,7 +26,7 @@ const InsertBoardForm = () => {
       title: "",
     } as InsertBoardArgs,
     onSubmit: async (data) => {
-      await signInMutation.mutateAsync(data.value);
+      await insertBoardMutation.mutateAsync(data.value);
     },
     validators: {
       onSubmit: InsertBoardSchema,
@@ -70,35 +38,35 @@ const InsertBoardForm = () => {
   };
 
   return (
-    <insertBoardForm.AppForm>
-      <form action={formAction} className="flex flex-col gap-4">
-        <FieldSet>
-          <FormError message={signInMutation.error?.message} />
-
-          <FieldGroup>
-            <insertBoardForm.AppField name="title">
-              {(field) => (
-                <Field data-invalid={!field.state.meta.isValid}>
-                  <FieldLabel>Title</FieldLabel>
-                  <field.Input placeholder="Title" required width="full" />
-                  <FieldError errors={field.state.meta.errors} />
-                </Field>
-              )}
-            </insertBoardForm.AppField>
-
-            <insertBoardForm.AppField name="description">
-              {(field) => (
-                <Field data-invalid={!field.state.meta.isValid}>
-                  <FieldLabel>Description</FieldLabel>
-                  <field.Input placeholder="Description" width="full" />
-                  <FieldError errors={field.state.meta.errors} />
-                </Field>
-              )}
-            </insertBoardForm.AppField>
-          </FieldGroup>
-        </FieldSet>
-        <insertBoardForm.Button type="submit">Insert</insertBoardForm.Button>
-      </form>
-    </insertBoardForm.AppForm>
+    <Dialog>
+      <DialogTrigger render={<Button variant="outline" />}>
+        Open Dialog
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-106.25">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <insertBoardForm.AppForm>
+          <form action={formAction} className="flex flex-col gap-4">
+            <BoardFields
+              error={insertBoardMutation.error}
+              form={insertBoardForm}
+            />
+            <insertBoardForm.Button type="submit">
+              Insert
+            </insertBoardForm.Button>
+          </form>
+        </insertBoardForm.AppForm>
+        <DialogFooter>
+          <DialogClose render={<Button variant="outline" />}>
+            Cancel
+          </DialogClose>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
